3c06b95454dfe52486775cc5ea404f3c28320e56
John Alarcon <j.alarcon@outlook.com>
2023-05-24 18:29:04 -0400

    created elements for displaying current data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var APIkey = "382a866a12d4771b986b0f7e34c597a5";
-var cities = []
+var cities = [];
 
 var cityInputEl = document.getElementById("city-input");
 var cityFormEl = document.getElementById("city-form");
@@ -41,3 +41,58 @@ var displaySearchedCities = function(city) {
 
     searchHistoryEl.appendChild(cityCardEl)
 }
+
+var displayCurrentData = function(city, data) {
+
+    var tempCurrent = Math.round(data.current.temp);
+    var humidity = Math.round(data.current.humility);
+    var windSpeed = data.current.wind_speed;
+    var uvIndex = data.current.uvi;
+    var iconCurrent = data.current.weather[0].icon;
+
+    currentContainerEl.textContent = "";
+    currentContainerEl.setAtrribute("class", "m-3 border col-10 text-center");
+    var divCityHeader = document.createElement("div");
+    var headerCityDate = document.createElement(h2);
+    var currentDate = moment().format("L");
+    var imageIcon = document.createElement("img");
+    imageIcon.setAttribute('src', "");
+    imageIcon.setAtttribute('src', "https://openweathermap.org/img/wn/" + iconCurrent + "@2x.png");
+    headerCityDate.textContent = city +" (" + currentDate + ")";
+    
+    divCityHeader.appendChild(headerCityDate)
+    divCityHeader.appendChild(imageIcon)
+    currentContainerEl.appendChild(divCityHeader)
+
+    var divCurrent = document.createElement("div");
+    var tempEl = document.createElement("p");
+    var humidityEl = document.createElement("p");
+    var windSpeedEl = document.createElement("p");
+    var uvIndexEl = document.createElement("p");
+    var uvIndexColorEl = document.createElement("span");
+
+    uvIndexColorEl.textContent = uvIndex
+
+        if (uvIndex <= 4) {
+            uvIndexColorEl.setAttribute("class", "bg-success text-white p-2")
+        } else if (uvIndex <= 8) {
+            uvIndexColorEl.setAttribute("class", "bg-warning text-black p-2")
+        } else {
+            uvIndexColorEl.setAttribute("class", "bg-danger text-white p-2")
+        }
+    
+    tempEl.textContent = "Temperature: " + tempCurrent + "°F";
+    humidityEl.textContent = "Humidity: " + humidity + "%";
+    windSpeedEl.textContent = "Wind Speed: " + windSpeed + " MPH";
+    uvIndexEl.textContent = "UV Index: ";
+
+    uvIndexEl.appendChjild(uvIndexColorEl)
+
+    divCurrent.appendChild(tempEl);
+    divCurrent.appendChild(humidityEl);
+    divCurrent.appendChild(windSpeedEl);
+    divCurrent.appendChild(uvIndexEl);
+
+    currentContainerEl.appendChild(divCurrent);
+
+};
